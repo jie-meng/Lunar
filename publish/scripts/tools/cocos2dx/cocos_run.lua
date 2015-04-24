ab50e7a2663d42ac5050978a481ea376afb85095
Joshua Meng <joshua_meng@trimble.com>
2015-04-24 18:09:57 +0800

    Modify cocos_run.lua

--- publish/scripts/tools/cocos2dx/cocos_run.lua
+++ publish/scripts/tools/cocos2dx/cocos_run.lua
@@ -2,5 +2,8 @@
 --os.execute("cocos compile -p win32")
 --os.execute("cocos compile -p linux")
 
+--run
 os.execute(file.currentPath() .. "/runtime/win32/LCocos.exe")
 --os.execute(file.currentPath() .. "/runtime/linux/LCocos")
+
+print("ok")
