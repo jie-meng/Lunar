5da627bf0df569be617ef39a009c15e4c885c69c
Joshua Meng <joshua_meng@trimble.com>
2015-04-27 14:34:18 +0800

    Refactor plugins/parse_supplement_api_lua.lua, support require module from current file dir

--- publish/plugins/parse_supplement_api_lua.lua
+++ publish/plugins/parse_supplement_api_lua.lua
@@ -1,5 +1,7 @@
 kRegexFunctionLua = "function\\s+(?<api>(\\w+((\\.|:)\\w+)*\\s*\\(.*\\)))"
 kRegexIncludeLua = "include\\(\"(?<path>.+)\"\\)"
+kRegexRequireLua = "(?<module>\\w+)\\s*=\\s*require\\(\"(?<path>.+)\"\\)"
+kRegexReturnModuleLua = "return\\s+(?<module>\\w+)"
 
 function parseSupplementApi(filename)
     
@@ -15,6 +17,7 @@ function parseSupplementApiRecursively(filename, dir, apis)
 
     local re_func = regex.create(kRegexFunctionLua)
     local re_inc = regex.create(kRegexIncludeLua)
+    local re_require = regex.create(kRegexRequireLua)
     
     local f = io.open(filename, "r")
     if f ~= nil then
@@ -32,6 +35,13 @@ function parseSupplementApiRecursively(filename, dir, apis)
                 if file.isPathFile(file_include) then
                     parseSupplementApiRecursively(file_include, dir, apis)
                 end
+            elseif regex.match(re_require, strTrim(line)) then
+                parseSupplementApiRequire(
+                    dir,
+                    regex.getMatchedGroupByName(re_require, "module"), 
+                    regex.getMatchedGroupByName(re_require, "path"), 
+                    apis,
+                    re_func)
             end
             line = f:read("*line")
         end
@@ -40,4 +50,58 @@ function parseSupplementApiRecursively(filename, dir, apis)
     
     regex.destroy(re_func)
     regex.destroy(re_inc)
+    regex.destroy(re_require)
+end
+
+function parseSupplementApiRequire(dir, module_name, require_name, api_t, re_func)
+    
+    local require_file = dir .. "/" .. require_name .. ".lua"
+    if not file.isPathFile(require_file) then
+        return
+    end
+    
+    local f = io.open(require_file, "r")
+    if f ~= nil then
+        local re_return_module = regex.create(kRegexReturnModuleLua)
+        local tmp_t = {}
+        local return_module = nil
+        
+        local line = f:read("*line")
+        while (line ~= nil) do
+            
+            if string.len(strTrim(line)) ~= 0 then
+                return_module = nil
+            end
+            
+            if regex.match(re_func, strTrim(line)) then
+                local api = regex.getMatchedGroupByName(re_func, "api")
+                if api ~= "" then
+                    local api_format, _ = string.gsub(api, ":", ".")
+                    table.insert(tmp_t, api_format)
+                end
+            elseif regex.match(re_return_module, strTrim(line)) then
+                return_module = regex.getMatchedGroupByName(re_return_module, "module")
+            end
+            
+            --next line
+            line = f:read("*line")
+        end
+        
+        if return_module ~= nil then
+            for k,v in pairs(tmp_t) do
+                if strStartWith(v, return_module .. ".") then
+                    table.insert(api_t, strReplace(v, return_module .. ".", module_name .. "."))
+                else
+                    table.insert(api_t, v)
+                end
+            end
+        else
+            for k,v in pairs(tmp_t) do
+                table.insert(api_t, v)
+            end
+        end
+
+        regex.destroy(re_return_module)
+        io.close(f)
+    end
 end
