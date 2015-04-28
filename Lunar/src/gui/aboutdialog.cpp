20959c0275b3ccacfe6d5a7364a890ee03516aca
Joshua Meng <joshua_meng@trimble.com>
2015-04-28 11:37:15 +0800

    remove useless buckup, update luax: remove include

--- Lunar/src/gui/aboutdialog.cpp
+++ Lunar/src/gui/aboutdialog.cpp
@@ -28,7 +28,7 @@ void AboutDialog::init()
 
 void AboutDialog::initGui()
 {
-    plabel_version_ = new QLabel(tr("Version: 1.2.1.20150425"));
+    plabel_version_ = new QLabel(tr("Version: 1.2.1.20150428"));
     plabel_email_ = new QLabel(tr("Email: joshua.meng.xy@gmail.com"));
 
     QVBoxLayout* pcenter_layout = new QVBoxLayout;
