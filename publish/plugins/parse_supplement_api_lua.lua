f828e9c855caf1eb8eb110fc988aeb82dc862aef
Joshua Meng <joshua_meng@trimble.com>
2015-04-28 11:12:09 +0800

    Refactor plugins/parse_supplement_api_lua.lua, remove include function, just use require now

--- publish/plugins/parse_supplement_api_lua.lua
+++ publish/plugins/parse_supplement_api_lua.lua
@@ -1,24 +1,27 @@
 kRegexFunctionLua = [[function\s+(?<api>(\w+((\.|:)\w+)*\s*\(.*\)))]]
-kRegexIncludeLua = [[include[\(\s]\s*\"(?<path>.+)\"\)?]]
-kRegexRequireLua = [[(?<module>\w+)\s*=\s*require[\(\s]\s*\"(?<path>.+)\"\)?]]
+kRegexRequireLua = [[((local\s+)?(?<module>\w+)\s*=\s*)?require[\(\s]\s*\"(?<path>.+)\"\)?]]
 kRegexReturnModuleLua = [[return\s+(?<module>\w+)]]
 
 function parseSupplementApi(filename)
     
+    local re_func = regex.create(kRegexFunctionLua)
+    local re_require = regex.create(kRegexRequireLua)
+    local re_return_module = regex.create(kRegexReturnModuleLua)
+    
     local dir, name = file.splitPathname(filename)
     local apis = {}
     
-    parseSupplementApiRecursively(filename, dir, apis)
+    parseSupplementApiMain(filename, dir, apis, re_func, re_require, re_return_module)
+    
+    regex.destroy(re_func)
+    regex.destroy(re_require)
+    regex.destroy(re_return_module)
     
     return apis
 end
 
-function parseSupplementApiRecursively(filename, dir, apis)
+function parseSupplementApiMain(filename, dir, apis, re_func, re_require, re_return_module)
 
-    local re_func = regex.create(kRegexFunctionLua)
-    local re_inc = regex.create(kRegexIncludeLua)
-    local re_require = regex.create(kRegexRequireLua)
-    
     local f = io.open(filename, "r")
     if f ~= nil then
         local line = f:read("*line")
@@ -29,30 +32,23 @@ function parseSupplementApiRecursively(filename, dir, apis)
                     local api_format, _ = string.gsub(api, ":", ".")
                     table.insert(apis, api_format)
                 end
-            elseif regex.match(re_inc, strTrim(line)) then
-                local file_include = dir .. "/" .. regex.getMatchedGroupByName(re_inc, "path")
-                if file.isPathFile(file_include) then
-                    parseSupplementApiRecursively(file_include, dir, apis)
-                end
             elseif regex.match(re_require, strTrim(line)) then
                 parseSupplementApiRequire(
                     dir,
                     regex.getMatchedGroupByName(re_require, "module"), 
                     regex.getMatchedGroupByName(re_require, "path"), 
                     apis,
-                    re_func)
+                    re_func,
+                    re_require,
+                    re_return_module)
             end
             line = f:read("*line")
         end
         io.close(f)
     end
-    
-    regex.destroy(re_func)
-    regex.destroy(re_inc)
-    regex.destroy(re_require)
 end
 
-function parseSupplementApiRequire(dir, module_name, require_name, api_t, re_func)
+function parseSupplementApiRequire(dir, module_name, require_name, api_t, re_func, re_require, re_return_module)
     
     if (not strContains(require_name, "/")) and (not strContains(require_name, "\\")) then
         require_name = strRelaceAll(require_name, ".", "/")
@@ -66,7 +62,6 @@ function parseSupplementApiRequire(dir, module_name, require_name, api_t, re_fun
     
     local f = io.open(require_file, "r")
     if f ~= nil then
-        local re_return_module = regex.create(kRegexReturnModuleLua)
         local tmp_t = {}
         local return_module = nil
         
@@ -74,7 +69,7 @@ function parseSupplementApiRequire(dir, module_name, require_name, api_t, re_fun
         while (line ~= nil) do
             
             if string.len(strTrim(line)) ~= 0 then
-                return_module = nil
+                return_module = ""
             end
             
             if regex.match(re_func, strTrim(line)) then
@@ -83,6 +78,16 @@ function parseSupplementApiRequire(dir, module_name, require_name, api_t, re_fun
                     local api_format, _ = string.gsub(api, ":", ".")
                     table.insert(tmp_t, api_format)
                 end
+            elseif regex.match(re_require, strTrim(line)) and (not strStartWith(strTrim(line), "local")) then
+                --local require in required file usually not return, just using locally, so ignore them.
+                parseSupplementApiRequire(
+                    dir,
+                    regex.getMatchedGroupByName(re_require, "module"), 
+                    regex.getMatchedGroupByName(re_require, "path"), 
+                    api_t,
+                    re_func,
+                    re_require,
+                    re_return_module)
             elseif regex.match(re_return_module, strTrim(line)) then
                 return_module = regex.getMatchedGroupByName(re_return_module, "module")
             end
@@ -91,10 +96,12 @@ function parseSupplementApiRequire(dir, module_name, require_name, api_t, re_fun
             line = f:read("*line")
         end
         
-        if return_module ~= nil then
+        if return_module ~= "" then
             for k,v in pairs(tmp_t) do
                 if strStartWith(v, return_module .. ".") then
-                    table.insert(api_t, strReplace(v, return_module .. ".", module_name .. "."))
+                    if module_name ~= "" then
+                        table.insert(api_t, strReplace(v, return_module .. ".", module_name .. "."))
+                    end
                 else
                     table.insert(api_t, v)
                 end
@@ -104,8 +111,7 @@ function parseSupplementApiRequire(dir, module_name, require_name, api_t, re_fun
                 table.insert(api_t, v)
             end
         end
-
-        regex.destroy(re_return_module)
+        
         io.close(f)
     end
 end
