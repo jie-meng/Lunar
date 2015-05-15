0f78765342b89af7619276525b3d37d5ea31c4a0
Joshua Meng <joshua_meng@trimble.com>
2015-05-15 09:31:31 +0800

    Refactor parse_cocos_api.lua, now auto api inherit relationship is ok

--- publish/scripts/tools/cocos2dx/parse_cocos_api.lua
+++ publish/scripts/tools/cocos2dx/parse_cocos_api.lua
@@ -2,9 +2,9 @@
 
 local ApiModule = {
     name_ = nil,
-    extend_ = nil,
     parent_ = nil,
     tmp_function_ = nil,
+    extend_ = {},
     functions_ = {}
 }
 
@@ -13,6 +13,7 @@ function ApiModule:new(o)
     setmetatable(o, self)
     self.__index = self
     
+    o.extend_ = {}
     o.functions_ = {}
     
     return o
@@ -20,6 +21,17 @@ end
 
 function ApiModule:getName()
     return self.name_
+end
+
+function ApiModule:getFullName()
+    local fullname = ""
+    if self.name_ then
+        fullname = self.name_
+        if self.parent_ then
+            fullname = self.parent_ .. "." .. fullname
+        end
+    end
+    return fullname
 end
 
 function ApiModule:getExtend()
@@ -35,7 +47,23 @@ function ApiModule:setName(name)
 end
 
 function ApiModule:setExtend(extend)
-    self.extend_ = extend
+    self.extend_ = strSplit(extend, ",")
+    for i, v in ipairs(self.extend_) do
+        self.extend_[i] = strTrim(v)
+    end
+end
+
+function ApiModule:addExtend(extend)
+    table.insert(self.extend_, extend)
+end
+
+function ApiModule:removeExtend(extend)
+    for i, v in ipairs(self.extend_) do
+        if v == extend then
+            table.remove(self.extend_, i)
+            break
+        end
+    end
 end
 
 function ApiModule:setParent(parent)
@@ -44,6 +72,10 @@ end
 
 function ApiModule:getFunctions()
     return self.functions_
+end
+
+function ApiModule:addFunction(func)
+    table.insert(self.functions_, func)
 end
 
 function ApiModule:StartFunc(func)
@@ -65,7 +97,7 @@ end
 
 function ApiModule:EndFunc(return_obj, return_type)
     if self.tmp_function_ then
-        table.insert(self.functions_, self.tmp_function_ .. ") -> " .. return_obj .. " " .. return_type)
+        table.insert(self.functions_, string.format("%s) --> %s (%s)", self.tmp_function_, return_type, return_obj))
         self.tmp_function_ = nil
         return true
     else
@@ -96,21 +128,21 @@ function createCocosApis()
     end
     
     -- auto api
+    local classes = {}
     for k, v in pairs(t3) do
         print("parse file " .. v)
         local api_module = parseAutoApi(v)
-        local functions = api_module:getFunctions()
-        local prefix = ""
-        if api_module:getName() then
-            prefix = api_module:getName() .. "."
-            if api_module:getParent() then
-                prefix = api_module:getParent() .. "." .. prefix
-            end
-        end
-        
-        for _, v in pairs(functions) do
-            table.insert(apis, prefix .. v)
-            --print(prefix .. v)
+        -- keep a record for inherit process
+        classes[api_module:getFullName()] = api_module
+    end
+    
+    -- process inherit
+    local processed_classes = processClassInherits(classes)
+    --local processed_classes = classes
+    -- add functions
+    for k, v in pairs(processed_classes) do
+        for i, f in ipairs(v:getFunctions()) do
+            table.insert(apis, k .. "." .. f)
         end
     end
     
@@ -143,13 +175,17 @@ function parseApi(filename, apis)
         local line = f:read("*line")
         while (line ~= nil) do
             repeat
-                local tb, func, param = string.match(strTrim(line), pattern_tb_function_lua)
+                if strTrim(line) == "" or strStartWith(strTrimLeft(line), "--") then
+                    break
+                end
+            
+                local tb, func, param = string.match(line, pattern_tb_function_lua)
                 if tb and func and param then
                     table.insert(apis, string.format("%s.%s%s", tb, func, param))
                     break
                 end
                 
-                func, param = string.match(strTrim(line), pattern_function_lua)
+                func, param = string.match(line, pattern_function_lua)
                 if func and param then
                     table.insert(apis, string.format("%s%s", func, param))
                     break
@@ -166,7 +202,7 @@ end
 function parseAutoApi(filename)
     
     local pattern_module = [[--%s*@module%s+([%w_]+)]]
-    local pattern_extend = [[--%s*@extend%s+([%w_]+)]]
+    local pattern_extend = [[--%s*@extend%s+([%w%s_,]+)]]
     local pattern_parent_module = [[--%s*@parent_module%s+([%w_]+)]]
     local pattern_function = [[--%s*@function%s+%[.+%]%s+([%w_]+)]]
     local pattern_param = [[--%s*@param%s+#(.+)]]
@@ -179,25 +215,28 @@ function parseAutoApi(filename)
         local line = f:read("*line")
         while (line ~= nil) do
             repeat
-            
-                local test_function = string.match(strTrim(line), pattern_function)
+                if strTrim(line) == "" then
+                    break
+                end
+                
+                local test_function = string.match(line, pattern_function)
                 if test_function then
                     api_module:StartFunc(test_function)
                 end 
                 
-                local test_param = string.match(strTrim(line), pattern_param)
+                local test_param = string.match(line, pattern_param)
                 if test_param then
                     api_module:AddParamToFunc(test_param)
                     break
                 end
                 
-                local test_return_obj, test_return_type = string.match(strTrim(line), pattern_return)
+                local test_return_obj, test_return_type = string.match(line, pattern_return)
                 if test_return_obj and test_return_type then
                     api_module:EndFunc(test_return_obj, test_return_type)
                     break
                 end 
                 
-                local test_module = string.match(strTrim(line), pattern_module)
+                local test_module = string.match(line, pattern_module)
                 if test_module then                    
                     api_module:setName(test_module)
                     break
@@ -205,11 +244,12 @@ function parseAutoApi(filename)
                 
                 local test_extend = string.match(strTrim(line), pattern_extend)
                 if test_extend then
+                    --print(test_extend)
                     api_module:setExtend(test_extend)
                     break
                 end
                 
-                local test_parent_module = string.match(strTrim(line), pattern_parent_module)
+                local test_parent_module = string.match(line, pattern_parent_module)
                 if test_parent_module then
                     api_module:setParent(test_parent_module)
                     break
@@ -245,7 +285,11 @@ function parseManualApi(filename)
         local line = f:read("*line")
         while (line ~= nil) do
             repeat
-                local tolua_function = string.match(strTrim(line), pattern_tolua_function)
+                if strTrim(line) == "" or strStartWith(strTrimLeft(line), "--") then
+                    break
+                end
+            
+                local tolua_function = string.match(line, pattern_tolua_function)
                 if #begin_modules > 0 and tolua_function then
                     table.insert(tb, strJoin(begin_modules, ".") .. "." .. tolua_function .. "(?)")
                     push_string = nil
@@ -257,13 +301,13 @@ function parseManualApi(filename)
                     break
                 end
                 
-                local str = string.match(strTrim(line), pattern_pushstring)
+                local str = string.match(line, pattern_pushstring)
                 if str then
                     push_string = str    
                     break
                 end
                 
-                if strStartWith(strTrim(line), "lua_pushcfunction") then
+                if strStartWith(line, "lua_pushcfunction") then
                     if prefix_class and push_string then
                         table.insert(tb, prefix_class .. "." .. push_string .. "(?)")
                         push_string = nil
@@ -271,25 +315,25 @@ function parseManualApi(filename)
                     break
                 end
                 
-                if strStartWith(strTrim(line), "lua_pop") then
+                if strStartWith(line, "lua_pop") then
                     prefix_class = nil
                     push_string = nil
                     break
                 end
                 
-                local prefix, class = string.match(strTrim(line), pattern_class)
+                local prefix, class = string.match(line, pattern_class)
                 if prefix and class then
                     prefix_class = prefix .. "." .. class
                     push_string = nil
                     break
                 end
                 
-                local bm = string.match(strTrim(line), pattern_tolua_begin_module)
+                local bm = string.match(line, pattern_tolua_begin_module)
                 if bm then
                     table.insert(begin_modules, bm)
                 end
                 
-                local em = string.match(strTrim(line), pattern_tolua_end_module)
+                local em = string.match(line, pattern_tolua_end_module)
                 if em then
                     table.remove(begin_modules)
                 end
@@ -303,6 +347,61 @@ function parseManualApi(filename)
     end
     
     return tb
+end
+
+function processClassInherits(classes)
+    
+    local classes_processed = {}
+    
+    while true do
+        local left_count = 0
+        for k, v in pairs(classes) do
+            left_count = left_count + 1
+            
+            -- put base class into classes_processed
+            if #v:getExtend() == 0 then
+                classes_processed[k] = v
+                classes[k] = nil
+            else
+                for i, e in ipairs(v:getExtend()) do 
+                    repeat
+                    -- super_class is in classes_processed, process it
+                        local key = ""
+                        if v:getParent() then
+                            key = v:getParent() .. "." .. e
+                        else
+                            key = e
+                        end
+                    
+                        if classes_processed[key] then
+                            local functions = classes_processed[key]:getFunctions()
+                            for _, f in ipairs(functions) do
+                                v:addFunction(f)
+                            end
+                            --print("remove " .. key .. " from " .. v:getFullName())
+                            v:removeExtend(e)
+                            break
+                        end
+                        
+                        -- super_class still not put into classes_processed, keep it and wait for next loop
+                        if classes[key] then
+                            break
+                        end
+                        
+                        -- not extend from current classes, ignore it.
+                        v:removeExtend(e)
+                        
+                    until true
+                end
+            end
+        end -- main for
+        
+        if left_count == 0 then
+            break
+        end
+    end -- while
+    
+    return classes_processed
 end
 
 -- main
