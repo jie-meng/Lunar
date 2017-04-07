1d446f37cd75912ffe00af264593a80e7e73819d
Jie Meng <jiemeng@thoughtworks.com>
2017-04-08 00:16:44 +0800

    Issue#58: Fix 'make_lunar.lua' always show fail on mac bug

--- make_lunar.lua
+++ make_lunar.lua
@@ -1,13 +1,25 @@
 if util.strContains(util.platformInfo(), "unix", false) then
-    util.pathRemove(util.currentPath() .. "/Lunar")
-    os.execute("make")
-    if util.isPathExists(util.currentPath() .. "/Lunar") == true then
-        print("make success")
+    if util.strContains(util.platformInfo(), "macos", false) then
+        --mac
+        util.pathRemoveAll(util.currentPath() .. "/Lunar.app")
+        os.execute("make")
+        if util.isPathDir(util.currentPath() .. "/Lunar.app") == true then
+                print("make success")
+        -- else
+            print("make fail")
+        end
     else
-        print("make fail")
+        --unix
+        util.pathRemove(util.currentPath() .. "/Lunar")
+        os.execute("make")
+        if util.isPathExists(util.currentPath() .. "/Lunar") == true then
+            print("make success")
+        else
+            print("make fail")
+        end
     end
-    
 elseif util.strContains(util.platformInfo(), "windows", false) then
+    --windows
     util.pathRemove(util.currentPath() .. "/release/Lunar.exe")
     os.execute("mingw32-make")
 
@@ -16,6 +28,4 @@ elseif util.strContains(util.platformInfo(), "windows", false) then
     else
         print("make fail")
     end
-else
-
 end
