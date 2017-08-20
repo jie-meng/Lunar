18c236e9eeac7024775f9186ab99e487e6089b04
Jie Meng <jiemeng@thoughtworks.com>
2017-08-20 22:44:19 +0800

    Rename table_ext functions

--- publish/extension_tools/cocos_lua_generate_get_set.lua
+++ publish/extension_tools/cocos_lua_generate_get_set.lua
@@ -26,12 +26,12 @@ file_ext.foreachLine(filepath,
             
             local field = string.match(trimmed_line, pattern_field)
             if field then
-                local parts = table_ext.filtercopy(util.strSplit(field, '_'), 
+                local parts = table_ext.filterCopy(util.strSplit(field, '_'), 
                     function (v)
                         return string.len(v) > 0
                     end)
                     
-                local func_name = util.strJoin(table_ext.mapcopy(parts,
+                local func_name = util.strJoin(table_ext.mapCopy(parts,
                     function (v)
                         return string.upper(string.sub(v, 1, 1)) .. string.sub(v, 2)
                     end), '')
@@ -49,7 +49,7 @@ file_ext.foreachLine(filepath,
     end)
 
 
-local gets_sets = table_ext.mapcopy(fields,
+local gets_sets = table_ext.mapCopy(fields,
     function (v)
         return string.format('function get%s()\n    return self.%s_\nend\n\nfunction set%s(%s)\n    self.%s_ = %s\n    return self\nend', 
             v.func, v.var, v.func, v.var, v.var, v.var)
