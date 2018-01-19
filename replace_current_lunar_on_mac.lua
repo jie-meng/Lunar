b7c0d5a066702f157d46c6fb4ce8e4bbf5390c85
Jie Meng <jiemeng@thoughtworks.com>
2018-01-19 14:22:26 +0800

    Copy pydoc_gen when replace_current_lunar_on_mac

--- replace_current_lunar_on_mac.lua
+++ replace_current_lunar_on_mac.lua
@@ -4,11 +4,17 @@ os.execute('cp /Applications/Lunar.app/Contents/MacOS/lunar.cfg Lunar.app/Conten
 if util.isPathFile('/Applications/Lunar.app/Contents/MacOS/apis/python/pydoclibs') then
     print('copy pydoclibs')
     os.execute('cp /Applications/Lunar.app/Contents/MacOS/apis/python/pydoclibs Lunar.app/Contents/MacOS/apis/python/')
+    if util.isPathExists('/Applications/Lunar.app/Contents/MacOS/apis/python/pydoc_gen') then
+        os.execute('mv /Applications/Lunar.app/Contents/MacOS/apis/python/pydoc_gen Lunar.app/Contents/MacOS/apis/python/')
+    end
 end
 
 if util.isPathFile('/Applications/Lunar.app/Contents/MacOS/apis/python/pydoclibs3') then
     print('copy pydoclibs3')
     os.execute('cp /Applications/Lunar.app/Contents/MacOS/apis/python/pydoclibs3 Lunar.app/Contents/MacOS/apis/python/')
+    if util.isPathExists('/Applications/Lunar.app/Contents/MacOS/apis/python/pydoc_gen3') then
+        os.execute('mv /Applications/Lunar.app/Contents/MacOS/apis/python/pydoc_gen3 Lunar.app/Contents/MacOS/apis/python/')
+    end
 end
 
 print('rm /Applications/Lunar.app')
