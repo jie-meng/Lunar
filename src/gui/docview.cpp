513218af240f57f4f5087c2ca4c12ee6ba2df149
Jie Meng <jiemeng@thoughtworks.com>
2018-04-17 11:29:38 +0800

    Fix window codeTemplate postion incorrect bug

--- src/gui/docview.cpp
+++ src/gui/docview.cpp
@@ -955,8 +955,14 @@ void DocView::codeTemplate()
             int end = 0;
             if (Extension::getInstance().templateFileInfo(it->second, index - (end_pos - start_pos), template_content, begin, end))
             {
+                int offset = 0;
+                if (platformInfo() == kPlatformWindows) {
+                    //On windows start_pos is not same as Unix, might caused by end of line '\r\n' not '\n'.
+                    offset = line;
+                }
+
                 ptext_edit_->replaceSelectedText(StdStringToQString(template_content));
-                ptext_edit_->setSelection(0, start_pos + begin, 0, start_pos + end);
+                ptext_edit_->setSelection(0, start_pos - offset + begin, 0, start_pos - offset + end);
             }
         }
     }
