d78984e278d6aefc58022afc335030c71845030d
Jie Meng <jiemeng@thoughtworks.com>
2018-04-25 10:17:52 +0800

    Make dockerfile match easily

--- publish/extension.lua
+++ publish/extension.lua
@@ -185,7 +185,7 @@ function parseFileType(filename)
 		return { type = "cmake", comment_line = "#", api = "apis/cmake", executor = "cmake" }
 	end
 
-    if util.fileBaseName(name) == "Dockerfile" then
+    if util.strContains(util.fileBaseName(name), "Dockerfile") then
 		return { type = "", comment_line = "#", api = "apis/dockerfile", executor = "" }
 	end
 
